9f97fe6f9091f17b237c2c1b3d499b99ef0fe330
opl- <opl-@users.noreply.github.com>
2022-08-10 00:07:24 +0200

    Use Adventure text API, deprecate Bungeecord chat

---
+++ src/main/java/com/mcmiddleearth/command/AdvStyle.java
@@ -0,0 +1,20 @@
+package com.mcmiddleearth.command;
+
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+
+public class AdvStyle {
+
+    public static final net.kyori.adventure.text.format.Style INFO = Style.style(NamedTextColor.AQUA);
+    public static final net.kyori.adventure.text.format.Style INFO_STRESSED = Style.style(NamedTextColor.GOLD);
+    public static final net.kyori.adventure.text.format.Style INFO_LIGHT = Style.style(NamedTextColor.YELLOW);
+    public static final net.kyori.adventure.text.format.Style ERROR = Style.style(NamedTextColor.RED);
+    public static final net.kyori.adventure.text.format.Style ERROR_STRESSED = Style.style(NamedTextColor.DARK_RED);
+    public static final net.kyori.adventure.text.format.Style LITERAL = Style.style(NamedTextColor.AQUA);
+    public static final net.kyori.adventure.text.format.Style ARGUMENT = Style.style(NamedTextColor.DARK_AQUA);
+    public static final net.kyori.adventure.text.format.Style HELP = Style.style(NamedTextColor.GREEN);
+    public static final net.kyori.adventure.text.format.Style TOOLTIP = Style.style(NamedTextColor.YELLOW);
+    public static final net.kyori.adventure.text.format.Style TOOLTIP_STRESSED = Style.style(NamedTextColor.GOLD);
+    public static final net.kyori.adventure.text.format.Style WARNING = Style.style(NamedTextColor.RED);
+
+}
--- src/main/java/com/mcmiddleearth/command/McmeCommandSender.java
+++ src/main/java/com/mcmiddleearth/command/McmeCommandSender.java
@@ -1,10 +1,17 @@
 package com.mcmiddleearth.command;
 
+import net.kyori.adventure.text.ComponentLike;
 import net.md_5.bungee.api.chat.BaseComponent;
 
 public interface McmeCommandSender {
 
+    /**
+     * @deprecated Use {@link #sendMessage(ComponentLike)} instead.
+     */
+    @Deprecated
     void sendMessage(BaseComponent[] message);
+
+    void sendMessage(ComponentLike message);
 
     default void sendMessage(String message) {}
 
